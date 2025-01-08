0b87e75965fbcc79f9f464bf23b63a7145f4ea41
Ankit Panda <ankitpanda2010@gmail.com>
2025-01-07 20:49:32 -0500

    Created Arm Subsystem, Defined in Robot Container and made constructor in subsystem

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -10,6 +10,7 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 import frc.robot.constants.Constants;
+import frc.robot.subsystems.ArmSubsystem;
 
 
 /**
@@ -19,12 +20,14 @@ import frc.robot.constants.Constants;
  * subsystems, commands, and trigger mappings) should be declared here.
  */
 public class RobotContainer {
+    public final ArmSubsystem arm = new ArmSubsystem();
 
     XboxController xboxController;
 
     /**
      * The container for the robot. Contains subsystems, OI devices, and commands.
      */
+
     public RobotContainer() {
         xboxController = new XboxController(Constants.XBOX_CONTROLLER_PORT);
         configureBindings();
--- src/main/java/frc/robot/subsystems/ArmSubsystem.java
+++ src/main/java/frc/robot/subsystems/ArmSubsystem.java
@@ -1,5 +1,25 @@
 package frc.robot.subsystems;
 
-public class ArmSubsystem {
+import com.ctre.phoenix6.configs.*;
+import com.ctre.phoenix6.hardware.CANcoder;
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.*;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+//import frc.robot.Constants;
 
+
+public class ArmSubsystem extends SubsystemBase {
+
+    private final TalonFX armKraken;
+    private final CANcoder armEncoder;
+
+    public ArmSubsystem() {
+        armKraken = new TalonFX(ArmConstants.ARM_KRAKEN_ID, Constants.TalonFXConstants.CANIVORE_NAME);
+        armEncoder = new CANcoder(ArmConstants.ARM_CANCODER_ID, Constants.TalonFXConstants.CANIVORE_NAME);
+    }
+
+    public static class ArmConstants {
+
+    }
 }
