ffdc61b50415381f53873f3ab872c5671898de04
Kartheek <kartheek.eadara@gmail.com>
2025-01-11 16:54:10 -0500

    i did feeder subsystem

---
+++ src/main/java/frc/robot/FeederSubsystem.java
@@ -0,0 +1,48 @@
+package frc.robot;
+
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.RunCommand;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import com.ctre.phoenix6.hardware.TalonFX;
+
+public class FeederSubsystem extends SubsystemBase {
+
+    // Creates a singleton
+    private final static FeederSubsystem INSTANCE = new FeederSubsystem();
+
+    // Motor Config
+    private static final int FEEDER_MOTOR_CAN_ID = 16;
+    private final TalonFX feederMotor;
+
+    //Beam Break Sensor Config
+    private static final int BEAM_BREAK_SENSOR_PORT = 2;
+    private final DigitalInput beamBreakSensor;
+
+    public static FeederSubsystem getInstance() {
+        return INSTANCE;
+    }
+
+    private FeederSubsystem() {
+        //References the motor and sensor as objects - "TalonFX & DigitalInput"
+        feederMotor = new TalonFX(FEEDER_MOTOR_CAN_ID);
+        beamBreakSensor = new DigitalInput(BEAM_BREAK_SENSOR_PORT);
+    }
+
+    private void setFeederSpeed(double speed) {
+        feederMotor.set(speed);
+    }
+
+    private void stopFeeder() {
+        setFeederSpeed(0);
+    }
+
+    private void feedBallToShooter() {
+        setFeederSpeed(1.0);
+    }
+
+    // Run the feeder for 3 seconds
+    public Command runFeederCommand(double speed) {
+        return run(() -> setFeederSpeed(speed)).withTimeout(3);
+    }
+}
--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,8 +1,3 @@
-// Copyright (c) FIRST and other WPILib contributors.
-
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot;
 
 import edu.wpi.first.wpilibj.XboxController;
@@ -10,45 +5,25 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 import frc.robot.constants.Constants;
+import frc.robot.FeederSubsystem;
+import edu.wpi.first.wpilibj2.command.RunCommand;
 
-
-/**
- * This class is where the bulk of the robot should be declared. Since Command-based is a
- * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
- * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
- * subsystems, commands, and trigger mappings) should be declared here.
- */
 public class RobotContainer {
 
-    XboxController xboxController;
+    CommandXboxController xboxController;
+    private final FeederSubsystem feederSubsystem;
 
-    /**
-     * The container for the robot. Contains subsystems, OI devices, and commands.
-     */
     public RobotContainer() {
-        xboxController = new XboxController(Constants.XBOX_CONTROLLER_PORT);
+        xboxController = new CommandXboxController(Constants.XBOX_CONTROLLER_PORT);
+        feederSubsystem = FeederSubsystem.getInstance();
         configureBindings();
     }
 
-
-    /**
-     * Use this method to define your trigger->command mappings. Triggers can be created via the
-     * {@link Trigger#Trigger(java.util.function.BooleanSupplier)} constructor with an arbitrary
-     * predicate, or via the named factories in {@link
-     * edu.wpi.first.wpilibj2.command.button.CommandGenericHID}'s subclasses for {@link
-     * CommandXboxController Xbox}/{@link edu.wpi.first.wpilibj2.command.button.CommandPS4Controller
-     * PS4} controllers or {@link edu.wpi.first.wpilibj2.command.button.CommandJoystick Flight
-     * joysticks}.
-     */
     private void configureBindings() {
-
+        // Sets the button of the feeder to be A
+        xboxController.a().onTrue(feederSubsystem.runFeederCommand(1));
     }
 
-    /**
-     * Use this to pass the autonomous command to the main {@link Robot} class.
-     *
-     * @return the command to run in autonomous
-     */
     public Command getAutonomousCommand() {
         return null;
     }
