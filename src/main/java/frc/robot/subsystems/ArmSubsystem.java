d20e1ecb3280ba3cdd542b2eeab871f8224a6fee
Ankit Panda <ankitpanda2010@gmail.com>
2025-01-16 18:47:17 -0500

    Fixed getEnc method

--- src/main/java/frc/robot/subsystems/ArmSubsystem.java
+++ src/main/java/frc/robot/subsystems/ArmSubsystem.java
@@ -4,9 +4,10 @@ import com.ctre.phoenix6.configs.*;
 import com.ctre.phoenix6.hardware.CANcoder;
 import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.signals.*;
+import edu.wpi.first.units.measure.Angle;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-//import frc.robot.Constants;
+import frc.robot.Constants;
 
 
 public class ArmSubsystem extends SubsystemBase {
@@ -17,15 +18,21 @@ public class ArmSubsystem extends SubsystemBase {
     public ArmSubsystem() {
         armKraken = new TalonFX(ArmConstants.ARM_KRAKEN_ID, Constants.TalonFXConstants.CANIVORE_NAME);
         armEncoder = new CANcoder(ArmConstants.ARM_CANCODER_ID, Constants.TalonFXConstants.CANIVORE_NAME);
+
+        var armConfigurator = armKraken.getConfigurator();
+        var talonFXConfiguration = new TalonFXConfiguration();
     }
 
     public static class ArmConstants {
         public static final int ARM_KRAKEN_ID = 21;
         public static final int ARM_CANCODER_ID = 5;
 
+        public static final InvertedValue ARM_INVERSION = InvertedValue.CounterClockwise_Positive;
+        public static final NeutralModeValue ARM_NEUTRAL_MODE = NeutralModeValue.Brake;
+
     }
 
-    public double getEnc() {
+    public Angle getEnc() {
         return armEncoder.getAbsolutePosition().getValue();
     }
 
