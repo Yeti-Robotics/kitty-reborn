2db1f59cb80ceb4f0682935e5083b361d20aadbc
Ankit Panda <ankitpanda2010@gmail.com>
2025-01-16 18:37:40 -0500

    Added Kraken and Cancoder IDs

--- src/main/java/frc/robot/subsystems/ArmSubsystem.java
+++ src/main/java/frc/robot/subsystems/ArmSubsystem.java
@@ -20,6 +20,37 @@ public class ArmSubsystem extends SubsystemBase {
     }
 
     public static class ArmConstants {
+        public static final int ARM_KRAKEN_ID = 21;
+        public static final int ARM_CANCODER_ID = 5;
 
+    }
+
+    public double getEnc() {
+        return armEncoder.getAbsolutePosition().getValue();
+    }
+
+    private void moveUp(double speed) {
+        armKraken.set(Math.abs(speed));
+    }
+
+    private void moveDown(double speed) {
+        armKraken.set(-Math.abs(speed));
+    }
+
+    public Command moveUpAndStop(double speed) {
+        return startEnd(() -> moveUp(speed), this::stop);
+    }
+
+    public Command moveDownAndStop(double speed){
+        return startEnd(() -> moveDown(speed), this::stop);
+    }
+
+    public Command deployArm(double speed){
+        return moveDownAndStop(speed).until(()
+                -> getEnc() <= ArmConstants.ARM_DEPLOY_UPPER_BOUND && getEnc() >= ArmConstants.ARM_DEPLOY_LOWER_BOUND);
+    }
+
+    private void stop() {
+        armKraken.stopMotor();
     }
 }
