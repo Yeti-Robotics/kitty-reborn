39dcfb9dc0b55db5a5488cc7bd6e7f22f9f17272
Ankit Panda <ankitpanda2010@gmail.com>
2025-01-24 18:46:22 -0500

    Config Object made in ArmSubsystemConfig.java

--- src/main/java/frc/robot/subsystems/ArmSubsystemConfig.java
+++ src/main/java/frc/robot/subsystems/ArmSubsystemConfig.java
@@ -1,7 +1,11 @@
 package frc.robot.subsystems;
 
+import com.ctre.phoenix6.configs.CANcoderConfiguration;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.signals.InvertedValue;
 import com.ctre.phoenix6.signals.NeutralModeValue;
+import com.ctre.phoenix6.hardware.CANcoder;
+import com.ctre.phoenix6.hardware.TalonFX;
 
 public class ArmSubsystemConfig {
     static final int ARM_KRAKEN_ID = 21;
@@ -14,4 +18,8 @@ public class ArmSubsystemConfig {
     static final double ARM_VELOCITY_STATUS_FRAME = 0.01;
     static final double ARM_HANDOFF_POSITION = 0.5;
     static final double ARM_DEPLOY_UPPER_BOUND = 0.15;
+    static final double ARM_DEPLOY_LOWER_BOUND = 0;
+
+    static final CANcoderConfiguration canconderconfigs = new CANcoderConfiguration();
+    static final TalonFXConfiguration talonfxconfigs = new TalonFXConfiguration();
 }
