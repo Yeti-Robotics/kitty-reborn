8abf9ba30a63ac4fa7f1467f7fc9ff00908ec560
Ankit Panda <ankitpanda2010@gmail.com>
2025-01-24 18:29:25 -0500

    Removed public from the constants in ArmSubsystemConfig.java

--- src/main/java/frc/robot/subsystems/ArmSubsystemConfig.java
+++ src/main/java/frc/robot/subsystems/ArmSubsystemConfig.java
@@ -3,17 +3,15 @@ package frc.robot.subsystems;
 import com.ctre.phoenix6.signals.InvertedValue;
 import com.ctre.phoenix6.signals.NeutralModeValue;
 
-
 public class ArmSubsystemConfig {
-    public static final int ARM_KRAKEN_ID = 21;
-    public static final int ARM_CANCODER_ID = 5;
+    static final int ARM_KRAKEN_ID = 21;
+    static final int ARM_CANCODER_ID = 5;
 
-    public static final InvertedValue ARM_INVERSION = InvertedValue.CounterClockwise_Positive;
-    public static final NeutralModeValue ARM_NEUTRAL_MODE = NeutralModeValue.Brake;
+    static final InvertedValue ARM_INVERSION = InvertedValue.CounterClockwise_Positive;
+    static final NeutralModeValue ARM_NEUTRAL_MODE = NeutralModeValue.Brake;
 
-    public static final double ARM_POSITION_STATUS_FRAME = 0.04;
-    public static final double ARM_VELOCITY_STATUS_FRAME = 0.01;
-    public static final double ARM_HANDOFF_POSITION = 0.5;
-    public static final double ARM_DEPLOY_UPPER_BOUND = 0.15;
-    public static final double ARM_DEPLOY_LOWER_BOUND = 0;
+    static final double ARM_POSITION_STATUS_FRAME = 0.04;
+    static final double ARM_VELOCITY_STATUS_FRAME = 0.01;
+    static final double ARM_HANDOFF_POSITION = 0.5;
+    static final double ARM_DEPLOY_UPPER_BOUND = 0.15;
 }
