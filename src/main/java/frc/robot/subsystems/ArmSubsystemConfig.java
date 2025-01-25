c23618f8792f1efc074d1d0a7352d481dc2a42c7
Ankit Panda <ankitpanda2010@gmail.com>
2025-01-25 16:36:12 -0500

    Removed Status Frames

--- src/main/java/frc/robot/subsystems/ArmSubsystemConfig.java
+++ src/main/java/frc/robot/subsystems/ArmSubsystemConfig.java
@@ -12,9 +12,6 @@ import com.ctre.phoenix6.signals.*;
 public class ArmSubsystemConfig {
     static final int ARM_KRAKEN_ID = 21;
     static final int ARM_CANCODER_ID = 5;
-
-    static final double ARM_POSITION_STATUS_FRAME = 0.04;
-    static final double ARM_VELOCITY_STATUS_FRAME = 0.01;
     static final double ARM_HANDOFF_POSITION = 0.5;
     static final double ARM_DEPLOY_UPPER_BOUND = 0.15;
     static final double ARM_DEPLOY_LOWER_BOUND = 0;
