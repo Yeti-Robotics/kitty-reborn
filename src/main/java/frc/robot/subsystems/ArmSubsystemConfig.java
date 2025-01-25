ab1b470c271207a7180a1ef079cb8d7d40472fc1
Ankit Panda <ankitpanda2010@gmail.com>
2025-01-25 15:19:50 -0500

    Deleted Constants

--- src/main/java/frc/robot/subsystems/ArmSubsystemConfig.java
+++ src/main/java/frc/robot/subsystems/ArmSubsystemConfig.java
@@ -12,9 +12,6 @@ public class ArmSubsystemConfig {
     static final int ARM_KRAKEN_ID = 21;
     static final int ARM_CANCODER_ID = 5;
 
-    static final InvertedValue ARM_INVERSION = InvertedValue.CounterClockwise_Positive;
-    static final NeutralModeValue ARM_NEUTRAL_MODE = NeutralModeValue.Brake;
-
     static final double ARM_POSITION_STATUS_FRAME = 0.04;
     static final double ARM_VELOCITY_STATUS_FRAME = 0.01;
     static final double ARM_HANDOFF_POSITION = 0.5;
@@ -25,7 +22,7 @@ public class ArmSubsystemConfig {
 
 
     static final CANcoderConfiguration canconderconfigs = new CANcoderConfiguration();
-    static final TalonFXConfiguration talonfxconfigs = new TalonFXConfiguration();
+    static final TalonFXConfiguration talonfxconfigs.slot0Configs = new TalonFXConfiguration();
 
     static final Slot0Configs slot0Configs = new Slot0Configs()
             .withKA(0)
