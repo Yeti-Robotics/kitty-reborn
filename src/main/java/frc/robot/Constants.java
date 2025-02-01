c0ca527272e06cd637ca343673856c928efa0714
Adharrsh <adharrsh153@gmail.com>
2025-02-01 14:43:32 -0500

    Made a new limelight system with limeliteHelpers

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -1,4 +1,4 @@
-package frc.robot.constants;
+package frc.robot;
 
 public class Constants {
     public static final int XBOX_CONTROLLER_PORT = 0;
--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -10,12 +10,10 @@ import com.ctre.phoenix6.swerve.SwerveRequest;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
-import frc.robot.constants.Constants;
 import frc.robot.generated.TunerConstants;
 import frc.robot.subsystems.CommandSwerveDrivetrain;
-
-import static frc.robot.constants.Constants.MaxAngularRate;
-import static frc.robot.constants.Constants.MaxSpeed;
+import static frc.robot.Constants.MaxAngularRate;
+import static frc.robot.Constants.MaxSpeed;
 
 
 /**
