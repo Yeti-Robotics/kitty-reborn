42e54121eec971913c58276140064f2939baf6ab
yetiDS <yeti.driverstation@gmail.com>
2025-02-22 22:13:34 -0500

    Added constants such as magnet offset rotor to sensor, etc.

--- src/main/java/frc/robot/subsystems/ArmSubsystemConfig.java
+++ src/main/java/frc/robot/subsystems/ArmSubsystemConfig.java
@@ -1,13 +1,16 @@
 package frc.robot.subsystems;
 
 import com.ctre.phoenix6.configs.*;
-import com.ctre.phoenix6.signals.GravityTypeValue;
-import com.ctre.phoenix6.signals.InvertedValue;
-import com.ctre.phoenix6.signals.NeutralModeValue;
+import com.ctre.phoenix6.signals.*;
 
 public class ArmSubsystemConfig {
     static final int ARM_KRAKEN_ID = 21;
     static final int ARM_CANCODER_ID = 5;
+    static final double SENSOR_TO_ROTOR = 60;
+    static final int SENSOR_TO_MECHANISM = 1;
+    static final Feed FEED_BACK_SENSOR_SOURCE = "Fused CANcoder";
+    static final double MAGNET_OFFSET = 0.442138671875;
+
     static final double ARM_HANDOFF_POSITION = 0.51;
     static final double ARM_DEPLOY_UPPER_BOUND = 0.15;
     static final double ARM_DEPLOY_LOWER_BOUND = 0;
@@ -18,6 +21,7 @@ public class ArmSubsystemConfig {
     static final double G_VALUE = 0;
     static final double S_VALUE = 0;
     static final double V_VALUE = 0.2;
+
 
     static final Slot0Configs slot0Configs = new Slot0Configs()
             .withGravityType(GravityTypeValue.Arm_Cosine)
@@ -43,12 +47,16 @@ public class ArmSubsystemConfig {
             .withSupplyCurrentLimitEnable(true)
             .withSupplyCurrentLimit(50);
 
-    static final CANcoderConfiguration canconderconfigs = new CANcoderConfiguration();
+    static final CANcoderConfiguration canconderconfigs = new CANcoderConfiguration()
+            .withMagnetSensor(new MagnetSensorConfigs().withMagnetOffset(0.442138671875).withSensorDirection(SensorDirectionValue.CounterClockwise_Positive).withAbsoluteSensorDiscontinuityPoint(.5));
 
     static final TalonFXConfiguration talonFXConfigs = new TalonFXConfiguration()
             .withSlot0(slot0Configs)
             .withMotionMagic(motionMagicConfigs)
             .withMotorOutput(motorOutputConfigs)
-            .withCurrentLimits(currentLimitsConfigs);
+            .withCurrentLimits(currentLimitsConfigs)
+            .withFeedback(new FeedbackConfigs().withFeedbackRemoteSensorID(ARM_CANCODER_ID).withFeedbackSensorSource(FeedbackSensorSourceValue.FusedCANcoder));
+
+
 
 }
